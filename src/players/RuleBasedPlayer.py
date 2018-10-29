e3aabef5df94ced3b4c79447973ef4ce76e7cfee
Shavarani <shavarani.sayyedhassan@gmail.com>
2018-10-29 01:24:32 -0700

    rulebased added

---
+++ src/players/RuleBasedPlayer.py
@@ -0,0 +1,38 @@
+import random
+from typing import Tuple, List
+from collections import Counter
+
+from dealer.Card import Card
+from dealer.Utils import GAMEMODE, SUITS
+from players.Player import Player, Bet
+
+
+class RuleBasedPlayer(Player):
+    def __init__(self, player_id, team_mate_player_id):
+        super(RuleBasedPlayer, self).__init__(player_id, team_mate_player_id)
+
+    # def make_bet(self, previous_last_bets: List[Bet]) -> Bet:
+    def select_saving_card_hand(self) -> Tuple[Tuple[int, int, int, int], GAMEMODE, SUITS]:
+        """
+        if its a hakem hand, selects 4 indices out of 16 and removes them out of hand and saves them in saved_deck 
+        :return: 
+        """
+        self.deck._cards = sorted(self.deck._cards)
+        cnts = Counter()
+        mappings = {}
+        for ind, card in enumerate(self.deck._cards):
+            cnts[card.suit] += 1
+            if card.suit not in mappings:
+                mappings[card.suit] = []
+            mappings[card.suit].append(ind)
+        hokm_suit = cnts.most_common()[0][0]
+        saving_hand = []
+        last_index = -1
+        while len(saving_hand) < 4:
+            lst = mappings[cnts.most_common()[last_index][0]]
+            saving_hand.append(lst.pop())
+            if not lst:
+                last_index -= 1
+        return tuple(saving_hand), self.game_mode, hokm_suit
+
+    # def play_a_card(self, hands_played: List[List[Card]], current_hand: List[Card]) -> Card:
--- test/GameTest.py
+++ test/GameTest.py
@@ -1,12 +1,25 @@
 import unittest
 
 from dealer.Game import Game
+from players.Player import Player
+from players.RuleBasedPlayer import RuleBasedPlayer
 
 
 class GameTester(unittest.TestCase):
     def setUp(self):
-        self.game = Game(True)
+        self.game = Game([Player(0, 2), Player(1, 3), Player(2, 0), Player(3, 1)], True)
 
-    def testARound(self):
-        s1, s2 = self.game.play_a_round()
-        print("Round {}: Team 1 score = {} and Team 2 score = {}".format(0, s1, s2))
+    # def testARound(self):
+        # s1, s2 = self.game.play_a_round()
+        # print("Round {}: Team 1 score = {} and Team 2 score = {}".format(0, s1, s2))
+
+    @staticmethod
+    def testRuleBasedPlayer():
+        gme = Game([RuleBasedPlayer(0, 2), Player(1, 3), RuleBasedPlayer(2, 0), Player(3, 1)], False)
+        s1, s2 = gme.play_a_round()
+        # print("Round {}: Team 1 score = {} and Team 2 score = {}".format(0, s1, s2))
+
+    @staticmethod
+    def testRuleBasedPlayer2():
+        gme = Game([RuleBasedPlayer(0, 2), Player(1, 3), RuleBasedPlayer(2, 0), Player(3, 1)], False)
+        gme.begin_game()
